b9b67cab50108dd7bf47d589957e653a36d3774f
Dmitry <82338843+Dmitry2310@users.noreply.github.com>
2022-05-13 23:28:08 +0900

    Create all  server requests

--- netflix-fake/src/Row.js
+++ netflix-fake/src/Row.js
@@ -1,7 +1,10 @@
 import instance from "./axios";
 import { useState, useEffect } from "react";
+import style from "./RowStyle.module.css";
 
-export const Row = ({ title, fetchUrl }) => {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+export const Row = ({ title, fetchUrl, isLargeRow }) => {
 
     const [movies, setMovies] = useState([]);
 
@@ -9,15 +12,23 @@ export const Row = ({ title, fetchUrl }) => {
         async function fetchData() {
             const request = await instance.get(fetchUrl);
             setMovies(request.data.results)
+            //console.log(movies);
             return request;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
 
     return (
-        <div>
+        <div className={style.row}>
             <h2>{title}</h2>
-
+            <div className={style.row__posters}>
+                {movies.map((movie) => (
+                    <img
+                        className={isLargeRow ? style.rowPosterLarge : style.row__poster}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                        alt={movie.name} key={movie.id} />
+                ))}
+            </div>
         </div>
     )
 }
